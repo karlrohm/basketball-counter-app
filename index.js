ed51c69c043455538aceb22fba24bfd20cebaa7a
karlrohm <karlthemountaingoat@gmail.com>
2022-11-16 12:33:49 +0200

    My first Java Script Project- Scrimba Basketball counter app

---
+++ index.js
@@ -0,0 +1,109 @@
+// let count = 0
+// let saveEl = document.getElementById("save-el")
+// let btn = document.getElementById("save-btn")
+
+
+
+
+// function increment() {
+//     count += 1
+//     document.getElementById("count").innerText = count
+// }
+
+// function save () {
+//     let countStr = count + " - "
+//     saveEl.textContent += countStr
+//     if (btn= true) {
+//         document.getElementById("count").innerText = count = 0 
+//     }
+// }
+
+
+// let name = "linda"
+// let greeting = "hey there, "
+
+// function messageGreeting (){
+//     console.log(greeting + "" + name + "!")
+// }
+// messageGreeting()
+
+
+// let myPoints = 0 
+// function add3points(){
+//    console.log( myPoints += 3)
+
+// }
+// function remove3points(){
+//    console.log(myPoints -= 3) 
+// }
+// add3points()
+// remove3points()
+
+
+// let num1 = 8 
+// let num2 = 2
+// document.getElementById("num1-el").textContent = num1
+// document.getElementById("num2-el").textContent = num2
+// let addition = num1 + num2
+// let subtraction = num1 - num2
+// let division = num1 / num2
+// let multiplication = num1 * num2
+
+// console.log(division, multiplication, addition , subtraction)
+// // Create four functions: add(), subtract(), divide(), multiply()
+// function add (){
+// document.getElementById("sum-el").textContent= addition
+// }
+// function subtract (){
+//     document.getElementById("sum-el").textContent= subtraction
+// }
+// function divide (){
+//     document.getElementById("sum-el").textContent= division
+// }
+
+// function multiply (){
+//     document.getElementById("sum-el").textContent= multiplication
+// }
+
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation using num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
+
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+
+let home =  0
+let away =  0
+
+// home scores
+function plusOne() {
+    document.getElementById("home-El").textContent=home +=1
+}
+function plusTwo() {
+    document.getElementById("home-El").textContent=home +=2
+}
+function plusThree() {
+    document.getElementById("home-El").textContent=home +=3
+}
+
+// away scores 
+function plus1() {
+    document.getElementById("guest-El").textContent=away +=1
+}
+function plus2() {
+    document.getElementById("guest-El").textContent=away +=2
+}
+function plus3() {
+    document.getElementById("guest-El").textContent=away +=3
+}
+document.getElementById(zero)
+console.log(zero)
+
+function newGame() {
+document.getElementById("guest-El").textContent=0
+document.getElementById("home-El").textContent= 0 
+console.log("a new game was started")
+}
+
+
+
